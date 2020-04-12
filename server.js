9a69028ca661a1b4011baaeecf5b5207951d68ca
Mike Fuller <emailthemike@gmail.com>
2020-04-12 01:23:52 -0400

    better errors and rules

--- server.js
+++ server.js
@@ -62,7 +62,6 @@ function Realtime(sock, auth=null, options = {
       if (users[user].username) {
         results[user].username = users[user].username;
       }
-      results[user].rooms = users[user].rooms;
     }
     c.send({"msgID":msgID, "type":"users", "users":results});
     return null;
@@ -72,7 +71,11 @@ function Realtime(sock, auth=null, options = {
     let msgID = m.msgID.toString() || null;
     if (m.room && typeof m.room === 'string') {
       if (!rooms[m.room]) {
-        c.send({"msgID":msgID, "error":"Room does not exist.", "room":m.room});
+        c.send({"msgID":msgID, "error":{"type":"room", "message":"Room does not exist.", "room":m.room}});
+        return null;
+      }
+      if (!rooms[m.room].users[c.id]) {
+        c.send({"msgID":msgID, "error":{"type":"room", "message":"You are not in this room.", "room":m.room}});
         return null;
       }
       c.send({
@@ -87,7 +90,11 @@ function Realtime(sock, auth=null, options = {
 
   const onListAllRooms = (c, m) => {
     let msgID = m.msgID.toString() || null;
-    c.send({"msgID":msgID, "type":"rooms", "rooms":rooms});
+    let list = [];
+    for(let i in rooms) {
+      list.push(i);
+    }
+    c.send({"msgID":msgID, "type":"rooms", "rooms":list});
   };
 
   const onJoin = (c, m) => {
@@ -98,7 +105,7 @@ function Realtime(sock, auth=null, options = {
       };
     }
     if (rooms[m.room].users[c.id]) {
-      c.send({"msgID":msgID, "error":"You are already in the room.", "room":m.room});
+      c.send({"msgID":msgID, "error":{"type":"join", "message":"You are already in the room.", "room":m.room}});
       return null;
     }
     rooms[m.room].users[c.id] = {"id":c.id, "username":users[c.id].username};
@@ -127,7 +134,7 @@ function Realtime(sock, auth=null, options = {
   const onLeave = (c, m) => {
     let msgID = m.msgID.toString() || null;
     if (!rooms[m.room] || !rooms[m.room].users[c.id]) {
-      c.send({"msgID":msgID, "error":"You are not in the room.","room":m.room});
+      c.send({"msgID":msgID, "error":{"type":"leave", "message":"You are not in the room.","room":m.room}});
       return null;
     }
     delete rooms[m.room].users[c.id];
@@ -155,7 +162,7 @@ function Realtime(sock, auth=null, options = {
   const onMessage = (c, m) => {
     let msgID = m.msgID.toString() || null;
     if (!users[m.to]) {
-      c.send({"msgID":msgID, "error":"The user does not exist.","to":m.to});
+      c.send({"msgID":msgID, "error":{"type":"message", "message":"The user does not exist.","to":m.to}});
       return null;
     } else {
       let timestamp = Date.now();
@@ -172,7 +179,7 @@ function Realtime(sock, auth=null, options = {
   const onChat = (c, m) => {
     let msgID = m.msgID.toString() || null;
     if (!rooms[m.room] || !rooms[m.room].users[c.id]) {
-      c.send({"msgID":msgID, "error":"You have not joined the room.", "room":m.room});
+      c.send({"msgID":msgID, "error":{"type":"chat", "message":"You have not joined the room.", "room":m.room}});
       return null;
     }
     let timestamp = Date.now();
@@ -204,7 +211,7 @@ function Realtime(sock, auth=null, options = {
     let msgID = m.msgID.toString() || null;
     if (m.id && typeof m.id === 'string') {
       if (!users[m.id]) {
-        c.send({"msgID":msgID, "error":"User does not exist.", "whois":m.id});
+        c.send({"msgID":msgID, "error":{"type":"whois", "message":"User does not exist.", "id":m.id}});
         return null;
       }
       c.send({
@@ -220,7 +227,7 @@ function Realtime(sock, auth=null, options = {
 
     if (m.username && typeof m.username === 'string') {
       if (!usernames[m.username]) {
-        c.send({"msgID":msgID, "error":"User does not exist.", "whois":m.username});
+        c.send({"msgID":msgID, "error":{"type":"whois", "message":"User does not exist.", "username":m.username}});
         return null;
       }
       c.send({
@@ -251,7 +258,7 @@ function Realtime(sock, auth=null, options = {
       }
       c.send({"msgID":msgID, "type":"auth", "auth":user.username});
     } else {
-      c.send({"msgID":msgID, "error":"Authentication token is expired or invalid."});
+      c.send({"msgID":msgID, "error":{"type":"auth", "message":"Authentication token is expired or invalid."}});
     }
   };
 
@@ -272,7 +279,7 @@ function Realtime(sock, auth=null, options = {
     let msgID = m.msgID.toString() || null;
 
     if (auth && !users[c.id].auth && m.type !== 'auth') {
-      c.send({"msgID":msgID, "error":"Authentication is required."});
+      c.send({"msgID":msgID, "error":{"type":"auth", "message":"Authentication is required."}});
       return null;
     }
 
@@ -326,7 +333,7 @@ function Realtime(sock, auth=null, options = {
       return null;
     }
 
-    c.send({"msgID":msgID, "error":"Request was invalid or restricted."});
+    c.send({"msgID":msgID, "error":{"type":"request", "message":"Request was invalid or restricted."}});
     return null;
 
   });
